271debb6cde715bf0d5dc75c6d5de4872b2bdf5f
ishaanchowdhary <ishaanguhachowdhary@gmail.com>
2024-08-19 18:30:24 +1000

    update week02-04
    added true map to test inside MCAV WS and modified speeds and aruco detector flag variable

--- Week02-04/operate.py
+++ Week02-04/operate.py
@@ -200,11 +200,11 @@ class Operate:
             ########### replace with your M1 codes ###########
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                self.command['motion'] = [2, 0]
+                self.command['motion'] = [1, 0]
                 self.ekf.origin = False
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                self.command['motion'] = [-2, 0]
+                self.command['motion'] = [-1, 0]
                 self.ekf.origin = False
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
